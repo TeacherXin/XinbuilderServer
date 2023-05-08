660b844b78e6db79f2cc97b7ac6b3b22fd9a696d
xuhexin <xuhxin@yonyou.com>
2023-05-08 16:51:50 +0800

    fix: 初始化项目

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,13 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { join } from 'path';
+import { NestExpressApplication } from '@nestjs/platform-express';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule,{ cors: true });
+  const app = await NestFactory.create<NestExpressApplication>(AppModule,{ cors: true });
+  app.useStaticAssets(join(__dirname, '..', 'public'), { prefix: '/' });  //设置虚拟路径
+  app.setBaseViewsDir(join(__dirname, '..', 'views')); // 放视图的文件
+  app.setViewEngine('ejs');
   await app.listen(3003);
 }
 bootstrap();
