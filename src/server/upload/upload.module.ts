560a0a0ade307b8f3258a45d23fbf55da145e470
xuhexin <xuhxin@yonyou.com>
2023-05-16 15:13:01 +0800

    fix: 修改一下图片路径问题

--- src/server/upload/upload.module.ts
+++ src/server/upload/upload.module.ts
@@ -12,7 +12,7 @@ import { join, extname } from 'path';
   imports: [MulterModule.register({
     //图片上传完要存放的位置
     storage: diskStorage({
-      destination: join(__dirname, '../images'),//存放的文件路径
+      destination: join('C:\\Myself\\React-cli\\XinbuilderServer\\public', 'images'),//存放的文件路径
       filename: (req, file, callback) => {
         //重新定义文件名，file.originalname 文件的原始名称
         // extname 获取文件后缀
